7db06551ce23dbdf4694482f2dbe30d41504f9a7
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-11-06 19:45:11 +0300

    Düzeltemeler

--- src/Assignments1/PrimeNumber/primeNumber.js
+++ src/Assignments1/PrimeNumber/primeNumber.js
@@ -1,18 +1,28 @@
-function isPrime(...params){
-    
-    for (let index = 0; index < params.length; index++) {
-        
-        if(params[index]%2==0 ){
-            console.log("PrimeNumbers : "+params[index])
-        }else if(params[index]%2==1){
-            console.log("NonPrimeNumbers : "+params[index])
-        }else{
-            console.log("Girdiğiniz değer sayısal bir değer değil :" + params[index])
-        }
-        
+function isPrime(...params) {
+
+  let count; //Bölen sayısı için
+
+  //Gönderilen elemanları gezecek döngü
+  for (let index = 0; index < params.length; index++) {
+    count = 0;
+    //Dizide gezilecek elemanları bölecek sayılar
+    for (let i = 2; i <= params[index]; i++) {
+      if (params[index] % i == 0) {
+        count++;//Dizide gezilen eleman i'deki değere tam bölünürse sayımız artacak.
+      }
     }
+
     
+    if (count == 2) {
+        //Asal olabilmesi için kendisine ve 1'e bölünebilmesi gerekiyor. Yani 2 sayıya bölünebilmeli
+      console.log("PrimeNumbers: " + params[index]);
+    } else if (count > 2) {
+        //2 den fazla sayıya bölünebiliyorsa yani kendinden başka sayılarada bölünebiliyorsa asal değildir.
+      console.log("NonPrime Numbers: " + params[index]);
+    }else if(count === 0){
+        console.log("Non Numeric")
+    }
+  }
 }
 
-
-isPrime(2,5,8,21,13,4,"test");
+isPrime(2, 5, 8, 21, 13, 4,999,997,"test",500);
--- src/Assignments1/PrimeNumber/primeNumber2.js
+++ src/Assignments1/PrimeNumber/primeNumber2.js
@@ -1,12 +1,21 @@
 function isPrimeNumber() {
 
-    for (let index = 2; index < 1000; index++) {
-        if(index%2==0){
-            console.log("Asal Sayılar : "+index);
+    // ilk döngümüz 1000'e kadar gidecek elemanlarımız için oluşuturulan döngü
+    for(let i = 2;i<=1000;i++){
+        let sayac = 0;//Bölen sayımızı tutacak değişken
+        for(let j=1;j<=i;j++){//1000'e kadar gidecek sayılarımızı bölecek döngü
+            if(i%j===0){
+                sayac++;
+            }
         }
-        
+        if(sayac===2){
+              //Asal olabilmesi için kendisine ve 1'e bölünebilmesi gerekiyor. Yani 2 sayıya bölünebilmeli
+            console.log("Asal Sayı: "+i);
+        }else{
+               //2 den fazla sayıya bölünebiliyorsa yani kendinden başka sayılarada bölünebiliyorsa asal değildir.
+            console.log("Asal Olmayan Sayı: "+i)
+        }
     }
-
     
 }
 
